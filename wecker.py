7ff7700b0d48a770437cca371d3c7e0fa2bd8397
einerix <einerix@icloud.com>
2017-08-22 15:58:46 +0200

    0.1
    menü und 1. test

---
+++ wecker.py
@@ -0,0 +1,14 @@
+import datetime
+import os
+import time
+
+while True:
+
+
+    ###menue###
+    print("enter your value")
+    print("1 for time")
+    menue_input = input(":~$")
+
+    if menue_input == 1:
+        print("true")
